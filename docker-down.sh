cdb324393e97f6dbe6df01ddfa0f409478b3c469
Dave <loki9421@gmail.com>
2018-05-24 12:10:50 +0100

    Added composer to PHP containers Dockerfile

--- docker-down.sh
+++ docker-down.sh
@@ -1,5 +1,17 @@
 #! bin/sh
+#find ~/ -iname docker-compose.yml
 
+#find ~/ -name .Trash -prune -o \( -iname docker-compose.yml -type d \)
+
+#dirs=($(find ~/ -name .Trash -prune -o \( -iname docker-compose.yml -type d \)))
+#for dir in "${dirs[@]}"; do
+#  cd "$dir"
+#  PWD=pwd
+#  echo $PWD
+#done
+
+# Working for finding dirs
+#find ~/ -name .Trash -prune -o \( -name docker-compose.yml \) -exec dirname {} \;
 dirs=$(find ~/ -name .Trash -prune -o \( -name docker-compose.yml -exec dirname {} \; \) )
 
 for d in $dirs
